696cd0b6d5feaacc518ca6e7ab14c2c07ded230d
Sam B <sbbaumo@yahoo.com>
2022-05-10 11:56:39 -0500

    initial

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+import React from 'react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <React.Fragment>
+        <Component {...pageProps} />
+      </React.Fragment>
+  );
+}
+
+export default MyApp
